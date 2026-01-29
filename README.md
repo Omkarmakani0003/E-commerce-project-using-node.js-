19d18ca1103d5dede4f476306e2b730f3cff1664
Omkarmakani0003 <makani.omkar3141@gmail.com>
2026-01-29 20:38:11 +0530

    remove project

--- README.md
+++
@@ -1,1 +0,0 @@
-E-commerce project using node js"# E-commerce-project-using-node.js--new-version" 
--- controllers/adminControllers/UserController.js
+++
@@ -1,7 +0,0 @@
-const { render } = require('ejs')
-const {user} = require('../../models/users')
-
-exports.users = async(req,res) => {
-    const users = await user.find() 
-    return res.render('admin/users',{users})
-}
